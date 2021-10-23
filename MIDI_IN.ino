359ea9d3aeec73518e802931bac530f3a20c1c21
Wayne <78999213+WayneSeng@users.noreply.github.com>
2021-10-23 02:19:24 -0700

    Add files via upload

--- MIDI_IN.ino
+++ MIDI_IN.ino
@@ -1,14 +1,15 @@
 byte incomingByte;
-int lsb;
-int msb;
 byte data;
 byte note;
 byte channel;
-byte velocity;
 byte bendMsb;
 byte bendLsb;
+byte controlNum;
+byte controlVal;
+int lsb;
+int msb;
 int action = -1;
-int bendVal = 8191;
+int bendVal = 0;
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -26,34 +27,39 @@ void loop () {
     else if (command == 0) { //data
       data = incomingByte;
     }
-    if (msb == 9 && action == -1) { // note on
+    if (msb == 9) { //note on
       channel = lsb;
       note = data;
-      action = 1;
-    }
-    else if (action == 1) { //receive velocity
-      velocity = data;
-      action = -1;
     }
     if (msb == 8) { //note off
       channel = lsb;
       note = data;
+    }
+    if (msb == 11 && action == -1) { //control change
+      channel = lsb;
+      controlNum = data;
+      action = 1;
+    }
+    else if (action == 1) { //receive controller value
+      controlVal = data;
+      action = -1;
     }
     if (msb == 14 && action == -1) { //pitch bend
       channel = lsb;
       bendLsb = data;
       action = 2;
     }
-    else if (action = 2) { //receive bendmsb
+    else if (action == 2) { //receive bendmsb
       bendMsb = data;
       bendVal = (bendMsb << 7) | bendLsb;
+      bendVal -= 8192;
       action = -1;
     }
   }
-  if(bendVal == 16383) {
+  if (bendVal == 8191) { //debugging
     digitalWrite(13, HIGH);
   }
-  else {
+  else if(bendVal == -8192) {
     digitalWrite(13, LOW);
   }
 }
