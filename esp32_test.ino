068ca56d3f5083439c481962468fce0f0270c249
Wayne <78999213+WayneSeng@users.noreply.github.com>
2022-04-05 18:54:13 -0700

    Add files via upload

--- esp32_test.ino
+++ esp32_test.ino
@@ -3,9 +3,26 @@ byte midiBendLsb;
 byte midiControlNum;
 byte midiAction = 0;
 byte offsetChannel;
+bool resetFlag;
 
-uint32_t stepper[] = {0, BIT4, BIT16, BIT17, BIT5}; //index 0 is not used!!!
-const int stepperLength = sizeof(stepper) / sizeof(stepper[0]);
+#define floppyChannels 2
+#define threshold1 64 //Sustain threshold 0-127
+#define threshold2 100
+#define threshold3 120
+unsigned int floppyData[] = {17, 15}; //data, latch, clock
+unsigned int floppyLatch[] = {5, 4}; //data, latch, clock
+unsigned int floppyClock[] = {18, 16}; //data, latch, clock
+byte floppyState[17];
+byte volume[17];
+byte sustainVal[17];
+bool floppyDir[17];
+unsigned long previousFloppy[17];
+unsigned int attackVal[17];
+unsigned int decayVal[17];
+
+#define stepperChannels 4
+unsigned int stepper[] = {21, 19, 32, 33};
+
 unsigned long currentTime;
 unsigned long previousTime[17];
 unsigned int currentPeriod[17];
@@ -17,12 +34,17 @@ float bendFactor[17];
 float originalBend[17];
 float maxBend[17];
 bool vibratoState[17];
+bool trill[17];
 
-unsigned long previousDrum[stepperLength];
-unsigned int drumDuration[stepperLength];
+#define drumCount 2
+unsigned int drums[] = {22, 23};
+unsigned long previousDrum[17];
+unsigned int drumDuration[17];
 
 hw_timer_t * timer = NULL;
+hw_timer_t * timer2 = NULL;
 volatile bool interruptbool1 = false;
+volatile bool interruptbool2 = false;
 
 const unsigned int pitchVals[128] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -42,42 +64,93 @@ void IRAM_ATTR onTimer() {
   interruptbool1 = true;
 }
 
+void IRAM_ATTR onTimer2() {
+  interruptbool2 = true;
+}
+
+void myShiftOut(uint8_t val, byte channel) {
+  uint8_t i;
+  digitalWrite(floppyLatch[channel - 5], LOW); //latch pin low
+  for (int i = 0; i < 8; i++) {
+    if (val & (1 << (7 - i))) {
+      digitalWrite(floppyData[channel - 5], HIGH); //data high
+    }
+    else {
+      digitalWrite(floppyData[channel - 5], LOW); //data low
+    }
+    digitalWrite(floppyClock[channel - 5], HIGH); //clock pin high
+    digitalWrite(floppyClock[channel - 5], LOW); //clock pin low
+  }
+  digitalWrite(floppyLatch[channel - 5], HIGH); //latch pin high
+}
+
 void setup() {
   Serial.begin(115200);
-  for (int i = 1; i < stepperLength; i++) {
-    REG_SET_BIT(GPIO_ENABLE_REG, stepper[i]);
+  for (int i = 0; i < stepperChannels; i++) {
+    pinMode(stepper[i], OUTPUT);
   }
-  for (int i = 1; i < 17; i++) {
-    bendFactor[i] = 1;
-    originalBend[i] = 1;
-    bendSens[i] = 2;
-    maxBend[i] = 0;
+  for (int i = 0; i < floppyChannels; i++) {
+    pinMode(floppyData[i], OUTPUT);
+    pinMode(floppyLatch[i], OUTPUT);
+    pinMode(floppyClock[i], OUTPUT);
   }
+  for (int i = 0; i < drumCount; i++) {
+    pinMode(drums[i], OUTPUT);
+  }
+  reset();
   timer = timerBegin(0, 80, true);
+  timer2 = timerBegin(1, 40, true);
   timerAttachInterrupt(timer, &onTimer, true);
+  timerAttachInterrupt(timer2, &onTimer2, true);
   timerAlarmWrite(timer, 80000, true);
+  timerAlarmWrite(timer2, 80000, true);
   timerAlarmEnable(timer);
+  timerAlarmEnable(timer2);
 }
 
 void loop() {
   readMIDI();
   currentTime = micros();
-  for (int i = 1; i < stepperLength; i++) {
+  for (int i = 1; i <= stepperChannels; i++) {
     if (currentNote[i] > 0 && currentTime - previousTime[i] >= currentPeriod[i]) {
       previousTime[i] = currentTime;
-      REG_SET_BIT(GPIO_OUT_W1TS_REG, stepper[i]);
-      REG_SET_BIT(GPIO_OUT_W1TC_REG, stepper[i]);
+      digitalWrite(stepper[i - 1], HIGH);
+      digitalWrite(stepper[i - 1], LOW);
     }
     if (drumDuration[i] > 0 && currentTime - previousDrum[i] >= 1000) {
       previousDrum[i] = currentTime;
       drumDuration[i]--;
-      REG_SET_BIT(GPIO_OUT_W1TS_REG, stepper[i]);
-      REG_SET_BIT(GPIO_OUT_W1TC_REG, stepper[i]);
+      digitalWrite(stepper[i - 1], HIGH);
+      digitalWrite(stepper[i - 1], LOW);
+    }
+  }
+  for (int i = 5; i < 5 + floppyChannels; i++) {
+    if (currentNote[i] > 0 && currentTime - previousTime[i] >= currentPeriod[i]) {
+      previousTime[i] = currentTime;
+      myShiftOut(floppyState[i] | B01010101, i);
+      myShiftOut(floppyState[i], i);
+      ADSR(i);
+      floppyDir[i] = !floppyDir[i];
+      if (floppyDir[i]) {
+        floppyState[i] |= B10101010;
+        myShiftOut(floppyState[i], i);
+      }
+      else {
+        floppyState[i] &= B01010101;
+        myShiftOut(floppyState[i], i);
+      }
+    }
+  }
+  for (int i = 5; i < 5 + drumCount; i++) {
+    if (drumDuration[i] > 0 && currentTime - previousDrum[i] >= drumDuration[i]) {
+      previousDrum[i] = currentTime;
+      drumDuration[i] = 0;
+      digitalWrite(drums[i - 5], LOW);
     }
   }
   if (interruptbool1) {
     interruptbool1 = false;
-    for (int i = 1; i < stepperLength; i++) {
+    for (int i = 1; i < 17; i++) {
       if (maxBend[i] > 0) {
         if (vibratoState[i]) {
           bendFactor[i] = originalBend[i] + maxBend[i];
@@ -90,6 +163,43 @@ void loop() {
       }
     }
   }
+  if (interruptbool2) {
+    interruptbool2 = false;
+    for (int i = 1; i < 17; i++) {
+      if (trill[i]) {
+        if (vibratoState[i]) {
+          currentPeriod[i] = pitchVals[currentNote[i] + 12] * bendFactor[i];
+        }
+        else {
+          currentPeriod[i] = pitchVals[currentNote[i]] * bendFactor[i];
+        }
+        vibratoState[i] = !vibratoState[i];
+      }
+    }
+  }
+}
+
+void ADSR(byte channel) {
+  //attack
+  if (currentTime - previousFloppy[channel] >= attackVal[channel] && volume[channel] >= threshold1) {
+    floppyState[channel] &= B11111011;
+  }
+  if (currentTime - previousFloppy[channel] >= attackVal[channel] * 2 && volume[channel] >= threshold2) {
+    floppyState[channel] &= B11101111;
+  }
+  if (currentTime - previousFloppy[channel] >= attackVal[channel] * 3 && volume[channel] >= threshold3) {
+    floppyState[channel] &= B10111111;
+  }
+  //decay
+  if (currentTime - previousFloppy[channel] >= (attackVal[channel] * 3) + decayVal[channel] && sustainVal[channel] <= threshold3) {
+    floppyState[channel] |= B01000000;
+  }
+  if (currentTime - previousFloppy[channel] >= (attackVal[channel] * 3) + (decayVal[channel] * 2) && sustainVal[channel] <= threshold2) {
+    floppyState[channel] |= B00010000;
+  }
+  if (currentTime - previousFloppy[channel] >= (attackVal[channel] * 3) + (decayVal[channel] * 3) && sustainVal[channel] <= threshold1) {
+    floppyState[channel] |= B00000100;
+  }
 }
 
 void handleNoteOn(byte channel, byte note, byte velocity) {
@@ -99,15 +209,27 @@ void handleNoteOn(byte channel, byte note, byte velocity) {
   else if (channel == 10) {
     if (note == 35 || note == 36) {
       drumDuration[1] = map(velocity, 1, 127, 1, 5);
+      drumDuration[5] = map(velocity, 1, 127, 1, 2000); //bass hdd
+      previousDrum[5] = currentTime;
+      digitalWrite(drums[0], HIGH);
     }
     else if (note == 38 || note == 40) {
       drumDuration[2] = map(velocity, 1, 127, 1, 10);
+      drumDuration[6] = map(velocity, 1, 127, 1, 1800); //snare hdd
+      previousDrum[6] = currentTime;
+      digitalWrite(drums[1], HIGH);
     }
     else {
       drumDuration[3] = map(velocity, 1, 127, 1, 3);
     }
   }
   else {
+    if (channel >= 5 && channel < 5 + floppyChannels) {
+      previousFloppy[channel] = currentTime;
+      floppyState[channel] |= B01010100;
+      volume[channel] = velocity;
+      resetFlag = true;
+    }
     currentNote[channel] = note;
     currentPeriod[channel] = pitchVals[note] * bendFactor[channel];
   }
@@ -116,6 +238,9 @@ void handleNoteOn(byte channel, byte note, byte velocity) {
 void handleNoteOff(byte channel, byte note, byte velocity) {
   if (note == currentNote[channel]) {
     currentNote[channel] = 0;
+    if (channel >= 5 && channel < 5 + floppyChannels) {
+      myShiftOut(floppyState[channel] | B01010101, channel);
+    }
   }
 }
 
@@ -130,7 +255,7 @@ void handlePitchBend(byte channel, int bend) {
 }
 
 void handleControlChange(byte channel, byte firstByte, byte secondByte) {
-  if (firstByte == 1) {
+  if (firstByte == 1) { //Modulation
     if (secondByte > 0) {
       maxBend[channel] = myMap(secondByte, 1, 127, 0, 0.03);
     }
@@ -139,6 +264,24 @@ void handleControlChange(byte channel, byte firstByte, byte secondByte) {
       bendFactor[channel] = originalBend[channel];
       currentPeriod[channel] = pitchVals[currentNote[channel]] * bendFactor[channel];
     }
+  }
+  if (firstByte == 12) { //floppy sustain
+    sustainVal[channel] = secondByte;
+  }
+  if (firstByte == 13) { //decay 
+    decayVal[channel] = map(secondByte, 0, 127, 0, 100000);
+  }
+  if (firstByte == 14) { //stepper special effect
+    if (secondByte > 0) {
+      trill[channel] = true;
+    }
+    else {
+      trill[channel] = false;
+      currentPeriod[channel] = pitchVals[currentNote[channel]] * bendFactor[channel];
+    }
+  }
+  if (firstByte == 15) { //attack
+    attackVal[channel] = map(secondByte, 0, 127, 0, 100000);
   }
   if (firstByte == 100) {
     controlVal1[channel] = secondByte;
@@ -151,10 +294,39 @@ void handleControlChange(byte channel, byte firstByte, byte secondByte) {
     controlVal1[channel] = 127;
     controlVal2[channel] = 127;
   }
+  if (firstByte == 121 && resetFlag) {
+    resetFlag = false;
+    reset();
+  }
 }
 
 float myMap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void reset() {
+  for (int i = 0; i < 17; i++) {
+    bendFactor[i] = 1;
+    originalBend[i] = 1;
+    bendSens[i] = 2;
+    maxBend[i] = 0;
+    currentNote[i] = 0;
+    attackVal[i] = 10000;
+    decayVal[i] = 30000;
+    sustainVal[i] = 0;
+  }
+  for (int i = 5; i < 5 + floppyChannels; i++) {
+    for (int j = 0; j < 160; j++) {
+      myShiftOut(B00000000, i);
+      myShiftOut(B01010101, i);
+      delay(5);
+    }
+    for (int k = 0; k < 40; k++) {
+      myShiftOut(B10101010, i);
+      myShiftOut(B11111111, i);
+      delay(5);
+    }
+  }
 }
 
 void readMIDI() {
